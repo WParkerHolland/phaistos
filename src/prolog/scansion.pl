8eeeecc3edb8e617532cd2200d2595289ff4ad70
William Holland <parkerholland04@gmail.com>
2024-08-22 08:48:44 -0400

    Complete prolog matching algorithm

--- src/prolog/scansion.pl
+++ src/prolog/scansion.pl
@@ -1,62 +1,27 @@
 % Prolog Algorithm containing rules to match generated scansions to patterns we believe are found on the disk
 
-% Theory for check index rule
-% checkIndex(S, P, I, L) This rule finds the index I of the pattern P of L length
-%   constructList(S, I, C, L) This rule recursively creates a list C of L length starting at I index
-%   P = C
+checkSpecIndex([H|_], 0, C) :- H = C.
+checkSpecIndex([_|T], I, C) :- I1 is I - 1, checkSpecIndex(T, I1, C).
 
-rightWay(S, I):- 
-    P = ['l','u','u','d','l','l','d','l','x'],
+rightWay(S, I):-
     append([_, P, _], S),
-    checkIndex(S, P, I, L),
-    L = 9.
-rightWay(S, I):- 
-    P = ['l','l','d','l','l','d','l','x'],
-    append([_, P, _], S),
-    checkIndex(S, P, I, L),
-    L = 8.
+    L = [['l','u','u','d','l','l','d','l','x'], ['l','l','d','l','l','d','l','x']],
+    member(P, L),
+    checkSpecIndex(L, I, P).
 
-
-wrongWay(S, I):- 
-    P = ['x','l','l','d','l','l','d','l','l','d'],
-    append([_, P, _], S),
-    checkIndex(S, P, I, L),
-    L = 10.
-wrongWay(S, I):- 
-    P = ['l','l','d','l','l','d','l','l','d'],
+wrongWay(S, I):-
     append([P, _], S),
-    checkIndex(S, P, I, L),
-    L = 9.
-
-wrongWay(S, I):- 
-    P = ['x','l','s','s','d','l','l','d','l','l','d'],
+    L = [['l','l','d','l','l','d','l','l','d'], 
+        ['l','s','s','d','l','l','d','l','l','d'], 
+        ['l','s','s','d','l','l','d','l','s','s','d'],
+        ['l','l','d','l','l','d','l','s','s','d']],
+    member(P, L),
+    checkSpecIndex(L, I, P).
+wrongWay(S, I):-
     append([_, P, _], S),
-    checkIndex(S, P, I, L),
-    L = 11.
-wrongWay(S, I):- 
-    P = ['l','s','s','d','l','l','d','l','l','d'],
-    append([P, _], S),
-    checkIndex(S, P, I, L),
-    L = 10.
-
-wrongWay(S, I):- 
-    P = ['x','l','s','s','d','l','l','d','l','s','s','d'],
-    append([_, P, _], S),
-    checkIndex(S, P, I, L),
-    L = 12.
-wrongWay(S, I):- 
-    P = ['l','s','s','d','l','l','d','l','s','s','d'],
-    append([P, _], S),
-    checkIndex(S, P, I, L),
-    L = 11.
-
-wrongWay(S, I):- 
-    P = ['x','l','l','d','l','l','d','l','s','s','d'],
-    append([_, P, _], S),
-    checkIndex(S, P, I, L),
-    L = 11.
-wrongWay(S, I):- 
-    P = ['l','l','d','l','l','d','l','s','s','d'],
-    append([P, _], S),
-    checkIndex(S, P, I, L),
-    L = 10.
+    L = [['x','l','l','d','l','l','d','l','l','d'], 
+        ['x','l','s','s','d','l','l','d','l','l','d'], 
+        ['x','l','s','s','d','l','l','d','l','s','s','d'],
+        ['x','l','l','d','l','l','d','l','s','s','d']],
+    member(P, L),
+    checkSpecIndex(L, I, P).
